67dbe1cd25eea6a02c13446dd07f4585afb33273
schauerstoff <ss.schmidt@gmx.de>
2021-05-25 19:39:11 +0200

    c'est pas assez

--- snatch_slides.py
+++ snatch_slides.py
@@ -1,3 +1,5 @@
+from docx import Document
+from docx.shared import Inches
 import sys
 import os
 from enum import Enum
@@ -20,67 +22,67 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy import signal
 
+from selenium.webdriver import ActionChains
+
 #!IMPORTANT, DOESNT WORK IF NOT DONE:
 # #Einstellungen > System > Anzeige > Skalierung und Anordnung: Groesse von Text und ...: 100% instead of 125%
 # wenn Screenshots sonst rechts abgeschnitten sind!
 
 # get the id of sth called "<video id=".." ..>, not easily found by inspector, open div boxes until found.
-# change paths here
+# change paths, ids here
 path = "C:\Program Files (x86)\chromedriver.exe"
 
-# Get Data from Video
-# # make error go away :-)
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
-# # kein chrome fenster wird geoeffnet
-# # options.add_argument("--headless")
-# driver = webdriver.Chrome(path, options=options)
-# driver.set_window_position(0, 0)
-# driver.set_window_size(1024, 768)
-# driver.get('https://videoakademie.ko-ld.de/Panopto/Pages/Viewer.aspx?id=20dc555a-84e3-4ca8-b520-abca00eeb191&start=undefined')
+# region SETUP
 
+
+# def setup():
+# make error go away :-)
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# kein chrome fenster wird geoeffnet
+# options.add_argument("--headless")
+driver = webdriver.Chrome(path, options=options)
+driver.set_window_position(0, 0)
+driver.set_window_size(1024, 768)
+driver.get('https://videoakademie.ko-ld.de/Panopto/Pages/Viewer.aspx?id=20dc555a-84e3-4ca8-b520-abca00eeb191&start=undefined')
 # Start Playing the Video at 2x speed
-# get video length, while 1/2 diese zeit noch nicht vergangen ist, do these
+play = driver.find_element_by_id('playButton')
+time.sleep(2)
+ActionChains(driver).click(play).perform()
+# Stummschalte!!
+speed = driver.find_element_by_id('playSpeedButton')
+ActionChains(driver).click(speed).perform()
+fastest = driver.find_element_by_id('Fastest')
+ActionChains(driver).click(fastest).perform()
 
-# element = driver.find_element_by_id('primaryVideo')
-# time.sleep(5)
-# element.screenshot('screenshot1.png')
-# time.sleep(5)
-# element.screenshot('screenshot2.png')
-# driver.quit()
-
-
-# compare
-# def load_image(infilename):
-#     img = Image.open(infilename)
-#     img.load()
-#     data = np.asarray(img, dtype="int32")
-#     return data
+element = driver.find_element_by_id('primaryVideo')
+document = Document()
 
 
-# def save_image(npdata, outfilename):
-#     img = Image.fromarray(np.asarray(
-#         np.clip(npdata, 0, 255), dtype="uint8"), "L")
-#     img.save(outfilename)
+# Paste in Word example
+# document = Document()
+# document.add_picture('screenshot1.png', width=Inches(6.0))
+# document.add_picture('screenshot2.png', width=Inches(6.0))
+# document.save('demo.docx')
+
+# endregion
+
+# region IMAGE PROCESSING
+
+# load as numpy array
 
 
-# s1 = load_image("screenshot1.png")
-# print(type(s1))
-# save_image(s1, "test.png")
+def load_image(infilename):
+    img = Image.open(infilename)
+    img.load()
+    data = np.asarray(img, dtype="int32")
+    return data
 
-def rgb2gray(rgb):
-    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
 
-
-img = mpimg.imread('screenshot1.png')
-s1 = rgb2gray(img)
-print(type(s1))
-# plt.imshow(gray, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
-# plt.show()
-img = mpimg.imread('screenshot2.png')
-s2 = rgb2gray(img)
-print("done")
-# NCC too slow
+def save_image(npdata, outfilename):
+    img = Image.fromarray(np.asarray(
+        np.clip(npdata, 0, 255), dtype="uint8"), "L")
+    img.save(outfilename)
 
 
 def normalize(arr):
@@ -89,17 +91,19 @@ def normalize(arr):
     return (arr-amin)*255/rng
 
 
-def main():
-    #file1, file2 = sys.argv[1:1+2]
-    # read images as 2D arrays (convert to grayscale for simplicity)
-    img1 = s2
-    img2 = s1
-    # compare
-    n_m, n_0 = compare_images(img1, img2)
-    print("Manhattan norm:", n_m)  # , "/ per pixel:", n_m/img1.size)
-    print("Zero norm:", n_0)  # , "/ per pixel:", n_0*1.0/img1.size)
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+# NCC too slow
 
 
+# Values for same picture, only moved cursor
+# Manhattan norm: 8325.966396323738
+# Zero norm: 138.0
+
+# Values for different pictures
+# Manhattan norm: 9913029.394698184
+# Zero norm: 158730.0
 def compare_images(img1, img2):
     # normalize to compensate for exposure difference, this may be unnecessary
     # consider disabling it
@@ -111,15 +115,39 @@ def compare_images(img1, img2):
     z_norm = norm(diff.ravel(), 0)  # Zero norm
     return (m_norm, z_norm)
 
+# endregion
 
-main()
+# region MAIN
+# get video length, while 1/2 diese zeit noch nicht vergangen ist, do these??
 
-# Values for same picture, only moved cursor
-# Manhattan norm: 8325.966396323738
-# Zero norm: 138.0
 
-# Values for different picture
-# Manhattan norm: 9913029.394698184
-# Zero norm: 158730.0
+time.sleep(10)
+# init prev
+element.screenshot('sc.png')
+prevprev = None  # vgl mit letzten beiden bildern!
+prev = rgb2gray(mpimg.imread('sc.png'))
+while True:
+    time.sleep(5)
+    element.screenshot('sc.png')
 
-#Paste in Word
+    tmp = rgb2gray(mpimg.imread('sc.png'))
+    n_m, n_0 = compare_images(prev, tmp)
+    print(n_0)
+    if(n_0 > 100000):  # threshold not quite gud yet
+        print("jaa")
+        document.add_picture('sc.png', width=Inches(6.0))
+        document.save('vl_am_date.docx')
+    prev = tmp
+
+# driver.quit()
+
+# img = mpimg.imread('screenshot1.png')
+# s1 = rgb2gray(img)
+# img = mpimg.imread('screenshot2.png')
+# s2 = rgb2gray(img)
+# img1 = s2
+#     img2 = s1
+#     # compare
+#     n_m, n_0 = compare_images(img1, img2)
+#     print("Manhattan norm:", n_m)  # , "/ per pixel:", n_m/img1.size)
+#     print("Zero norm:", n_0)  # , "/ per pixel:", n_0*1.0/img1.size)
